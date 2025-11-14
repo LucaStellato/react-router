96bd91b50dc87f2754ce279775ec9d080389c909
LucaStellato <lucastellato435@gmail.com>
2025-11-14 11:43:00 +0100

    import browser router

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import { useEffect } from 'react'
+import { BrowserRouter, browserRouter } from 'react-router-dom'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <BrowserRouter></BrowserRouter>
+    </>
+  )
+}
+
+export default App
