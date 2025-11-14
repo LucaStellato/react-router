7a2ea9408cdc1d8e8e62a4ca50dc600e437290a6
LucaStellato <lucastellato435@gmail.com>
2025-11-14 11:58:21 +0100

    create navBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import { useState } from 'react'
 import { useEffect } from 'react'
-import { BrowserRouter, browserRouter } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
+import HomePageLayout from './pages/HomePageLayout'
 
 function App() {
 
@@ -10,7 +11,7 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<HomePage />}></Route>
+          <Route path='/' element={<HomePageLayout />}></Route>
         </Routes>
       </BrowserRouter>
     </>
