3ce6fd9f6fa14f99dd761e5b6ef4c2c9dba4c215
LucaStellato <lucastellato435@gmail.com>
2025-11-14 12:48:44 +0100

    create default layout and create header file

---
+++ src/pages/DefaultLayout.jsx
@@ -0,0 +1,13 @@
+import { Outlet } from "react-router-dom";
+
+export default function DefaultLayout() {
+    return (
+        <>
+            <div>
+                <Header></Header>
+                <Outlet></Outlet>
+                <Footer></Footer>
+            </div>
+        </>
+    )
+}
