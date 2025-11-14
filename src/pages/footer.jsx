405405b7fab42f250cfe0efec1510ec2c2d3d058
LucaStellato <lucastellato435@gmail.com>
2025-11-14 12:51:15 +0100

    create footer component

---
+++ src/pages/footer.jsx
@@ -0,0 +1,14 @@
+
+
+export default function Footer() {
+    return (
+        <>        <footer style={{ backgroundColor: '#fff8f9', minHeight: '100px' }}>
+            <div className='row'>
+                <h4 className='mt-4' style={{ color: '#802c6e', textAlign: 'center' }}>se hai bisogno contattaci su:</h4>
+            </div>
+
+        </footer>
+        </>
+
+    )
+}
