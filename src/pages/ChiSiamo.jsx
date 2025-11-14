a172290c8a85af81d858b7192ed26a47a7e807e7
LucaStellato <lucastellato435@gmail.com>
2025-11-14 13:57:45 +0100

    page ChiSiamo complete

--- src/pages/ChiSiamo.jsx
+++ src/pages/ChiSiamo.jsx
@@ -1,6 +1,12 @@
 export default function ChiSiamo() {
     return (
         <>
+            <h3 className="mt-3" style={{ textAlign: 'center', color: '#802c6e' }}>Chi Siamo</h3>
+            <div className="" style={{ marginTop: '30px', marginBottom: '430px' }}>
+                <span className='text-secondary' style={{}}>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum a impedit esse commodi sed expedita nostrum ut nulla quae aut fugit vero corporis, accusantium quaerat labore voluptatem rerum sunt iusto perspiciatis id? Quia hic consequatur odio eius veritatis modi nulla praesentium, ipsum, aut amet, necessitatibus minima illo! Sit nesciunt aut accusamus non accusantium cupiditate excepturi possimus sed! Eum libero laboriosam repellat soluta, voluptate eligendi veritatis architecto neque quidem maiores ea, consequatur sapiente unde labore, quos ab numquam id culpa blanditiis voluptatibus expedita dicta? Dicta nemo iusto quibusdam praesentium iste deleniti, consequuntur quisquam porro culpa quos, voluptatum, accusantium suscipit ipsam adipisci?
+                </span>
+            </div>
         </>
     )
 }
