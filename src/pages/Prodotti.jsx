def6042100f1a0113acfebd60bc17ab31cd6439c
LucaStellato <lucastellato435@gmail.com>
2025-11-14 14:11:28 +0100

    create function

--- src/pages/Prodotti.jsx
+++ src/pages/Prodotti.jsx
@@ -4,7 +4,11 @@ import axios from 'axios'
 
 
 export default function Prodotti() {
-    const [products, setProducts] = useState([])
+    const [products, setProducts] = useState([]);
+    function fetchProducts() {
+        axios.get('https://fakestoreapi.com/products)')
+            .then((res) => setTodos(res.data))
+    }
     return (
         <>
 
