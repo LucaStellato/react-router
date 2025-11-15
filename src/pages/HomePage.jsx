bc3cb5dbaf528dcd8713310d83d8c7f269f91ffe
LucaStellato <lucastellato435@gmail.com>
2025-11-15 10:56:14 +0100

    stylizing button

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -7,9 +7,9 @@ export default function HomePageLayout() {
                     <img src={jmb}></img>
                     <h2 style={{ color: '#802c6e', marginTop: '100px' }}>Scegli ció che fa per te dal menú di navigazione</h2>
                     <div style={{
-                        position: 'relative', top: '260px', right: '480px'
+                        position: 'relative', top: '260px', right: '330px'
                     }}>
-                        <a class="btn btn-primary" href="/prodotti" role="button" > Link</a>
+                        <a class="btn btn" style={{ backgroundColor: '#802c6e', color: 'white', width: '260px' }} href="/prodotti" role="button" > Prodotti</a>
                     </div>
 
 
