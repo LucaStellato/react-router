66b41820ffd13724a185a224862c798dbec09bd7
LucaStellato <lucastellato435@gmail.com>
2025-11-15 10:51:21 +0100

    button position

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -6,9 +6,16 @@ export default function HomePageLayout() {
                 <div className='d-flex mt-4'>
                     <img src={jmb}></img>
                     <h2 style={{ color: '#802c6e', marginTop: '100px' }}>Scegli ció che fa per te dal menú di navigazione</h2>
+                    <div style={{
+                        position: 'relative', top: '260px', right: '480px'
+                    }}>
+                        <a class="btn btn-primary" href="/prodotti" role="button" > Link</a>
+                    </div>
 
-                </div>
-            </main>
+
+                </div >
+
+            </main >
         </>
     )
 }
