96e71ab967f7903c287e1fcf0e7dc3207ba5b897
LucaStellato <lucastellato435@gmail.com>
2025-11-15 10:41:09 +0100

    add bootstrap icons

--- src/pages/footer.jsx
+++ src/pages/footer.jsx
@@ -5,6 +5,13 @@ export default function Footer() {
         <>        <footer style={{ backgroundColor: '#fff8f9', minHeight: '100px' }}>
             <div className='row'>
                 <h4 className='mt-4' style={{ color: '#802c6e', textAlign: 'center' }}>se hai bisogno contattaci su:</h4>
+                <div>
+                    <i class="bi bi-envelope"></i>
+                    <i class="bi bi-facebook"></i>
+                    <i class="bi bi-twitter-x"></i>
+                    <i class="bi bi-instagram"></i>
+                    <i class="bi bi-telephone"></i>
+                </div>
             </div>
 
         </footer>
