9c2c13d8126992692d6eddb9a2464ed29ea0758c
LucaStellato <lucastellato435@gmail.com>
2025-11-15 11:33:28 +0100

    optimization

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -25,7 +25,7 @@ export default function Header() {
                     </div>
                 </div>
             </nav>
-        </header>
+        </header >
 
     )
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,9 +5,9 @@ export default function HomePageLayout() {
             <main className='' style={{}}>
                 <div className='d-flex mt-4'>
                     <img src={jmb}></img>
-                    <h2 style={{ color: '#802c6e', marginTop: '100px' }}>Scegli ció che fa per te dal menú di navigazione</h2>
+                    <h2 style={{ color: '#802c6e', marginTop: '100px', fontSize: '40px' }}>Scegli ció che fa per te dal menú di navigazione, oppure clicca qui</h2>
                     <div style={{
-                        position: 'relative', top: '260px', right: '330px'
+                        position: 'relative', top: '260px', right: '620px'
                     }}>
                         <a class="btn btn" style={{ backgroundColor: '#802c6e', color: 'white', width: '260px' }} href="/prodotti" role="button" > Prodotti</a>
                     </div>
