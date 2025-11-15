2b6ff05b354ea10a7e698934bfbcaac71f6da8b5
LucaStellato <lucastellato435@gmail.com>
2025-11-15 10:45:52 +0100

    stylizing icons

--- src/pages/footer.jsx
+++ src/pages/footer.jsx
@@ -5,12 +5,14 @@ export default function Footer() {
         <>        <footer style={{ backgroundColor: '#fff8f9', minHeight: '100px' }}>
             <div className='row'>
                 <h4 className='mt-4' style={{ color: '#802c6e', textAlign: 'center' }}>se hai bisogno contattaci su:</h4>
-                <div>
-                    <i class="bi bi-envelope"></i>
-                    <i class="bi bi-facebook"></i>
-                    <i class="bi bi-twitter-x"></i>
-                    <i class="bi bi-instagram"></i>
-                    <i class="bi bi-telephone"></i>
+                <div className="d-flex justify-content-center" style={{
+                    color: '#802c6e'
+                }}>
+                    <i class="bi bi-envelope me-3"></i>
+                    <i class="bi bi-facebook me-3"></i>
+                    <i class="bi bi-twitter-x me-3"></i>
+                    <i class="bi bi-instagram me-3"></i>
+                    <i class="bi bi-telephone me-3"></i>
                 </div>
             </div>
 
