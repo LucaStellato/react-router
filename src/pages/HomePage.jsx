6ef268a9a5c688cedc6e944e887189f9f4afc88e
LucaStellato <lucastellato435@gmail.com>
2025-11-16 14:02:23 +0100

    optimization layout homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,12 +4,14 @@ export default function HomePageLayout() {
         <>
             <main className='' style={{}}>
                 <div className='d-flex mt-4'>
-                    <img src={jmb}></img>
-                    <h2 style={{ color: '#802c6e', marginTop: '100px', fontSize: '40px' }}>Scegli ció che fa per te dal menú di navigazione, oppure clicca qui</h2>
-                    <div style={{
-                        position: 'relative', top: '260px', right: '620px'
-                    }}>
-                        <a class="btn btn" style={{ backgroundColor: '#802c6e', color: 'white', width: '260px' }} href="/prodotti" role="button" > Prodotti</a>
+                    <div>
+                        <img src={jmb}></img>
+                    </div>
+                    <div>
+                        <h2 style={{ color: '#802c6e', marginTop: '100px', fontSize: '40px' }}>Scegli ció che fa per te dal menú di navigazione, oppure clicca qui</h2>
+                        <a class="btn btn" style={{
+                            backgroundColor: '#802c6e', color: 'white', width: '260px', marginTop: '40px', marginLeft: '122px'
+                        }} href="/prodotti" role="button" > Prodotti</a>
                     </div>
 
 
