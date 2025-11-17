bb8e594cb1d0734fbfbc33277e63c8db0e1af728
LucaStellato <lucastellato435@gmail.com>
2025-11-17 10:59:42 +0100

    create const for single product

--- src/pages/singleproduct.jsx
+++ src/pages/singleproduct.jsx
@@ -3,6 +3,8 @@ import { useParams } from "react-router-dom";
 
 
 export default function singleProduct() {
+    const { id } = useParams();
+    const [product, setProduct] = useState({})
     return (
         <>
         </>
